8b9673db61226ded6d0d9254a33a58fcb6ea1fd4
jnvc583 <jnvcotc@zohomail.cn>
2025-06-29 14:03:19 +0800

    Create ilspf.js

---
+++ ilspf.js
@@ -0,0 +1,11 @@
+function ilspf(){
+  const sep = 0;
+  while(sep == 0){
+    setTimeout(() => {
+      document.getElementById('myImage').src='ilspf2.gif';
+    }, 2000);
+    setTimeout(() => {
+      document.getElementById('myImage').src='ilspf1.gif';
+    }, 2000);
+  }
+}
