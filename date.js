c7e43b57a97f5480be3fd44bb54654d962473bd3
Yunze Chen <jnvcotc@zohomail.cn>
2025-12-14 11:42:53 +0800

    Add files via upload

--- date.js
+++ date.js
@@ -1,9 +1,13 @@
+let avt = document.getElementById("avatar");
 function getCurrentDate() {
-const date = new Date();
-    let year = date.getFullYear();
+    const date = new Date();
     let month = String(date.getMonth() + 1).padStart(2, '0'); // 月份从0开始
     let day = String(date.getDate()).padStart(2, '0');
-    return `${year}-${month}-${day}`;
+    return `${month}-${day}`;
 }
-
 console.log(getCurrentDate());
+if (getCurrentDate() === "12-13" || getCurrentDate() === "12-14") {
+    document.documentElement.style.filter = "grayscale(100%)";
+    document.documentElement.style.webkitFilter = "grayscale(100%)";
+    avt.src = "./sadavatar.png";
+}
